dfba9b310f760a97cf0c85a445a8131abd2f65aa
Marck Vaisman <marck@vaisman.us>
2024-09-17 10:26:28 -0400

    update readme and fix polars script

--- benchmark-scripts/using_polars.py
+++ benchmark-scripts/using_polars.py
@@ -3,17 +3,23 @@ import polars as pl
 def create_polars_df():
     pl.Config.set_streaming_chunk_size(4000000)
     return (
-        
-        pl.scan_csv("../data/measurements.txt", separator=";", has_header=False, new_columns=["station", "measure"], schema={"station": pl.String, "measure": pl.Float64})
-        .group_by(by="station")
+        pl.scan_csv(
+            "../data/measurements.txt",
+            separator=";",
+            has_header=False,
+            new_columns=["station", "measure"],
+            schema={"station": pl.String, "measure": pl.Float64},
+        )
+        .group_by("station")
         .agg(
-            max = pl.col("measure").max(),
-            min = pl.col("measure").min(),
-            mean = pl.col("measure").mean()
+            max=pl.col("measure").max(),
+            min=pl.col("measure").min(),
+            mean=pl.col("measure").mean(),
         )
         .sort("station")
         .collect(streaming=True)
     )
+
 
 if __name__ == "__main__":
     import time
